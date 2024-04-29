a3710f16c69b571055c700c6e073f51019c663a1
ml1413 <ml1413sam@gmail.com>
2024-04-29 18:25:28 +0300

    fix bag picket time date

--- app/src/main/java/com/hutapp/org/notes/hut/android/MainActivity.kt
+++ app/src/main/java/com/hutapp/org/notes/hut/android/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
