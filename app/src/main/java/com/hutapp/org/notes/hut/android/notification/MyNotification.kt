557f9c75132dcc34fc25aba9f9fa08349908f7b1
ml1413 <ml1413sam@gmail.com>
2024-04-30 15:03:03 +0300

    add tap on notiffication

--- app/src/main/java/com/hutapp/org/notes/hut/android/notification/MyNotification.kt
+++ app/src/main/java/com/hutapp/org/notes/hut/android/notification/MyNotification.kt
@@ -2,35 +2,39 @@ package com.hutapp.org.notes.hut.android.notification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
-import android.os.Build
+import android.content.Intent
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
+import com.hutapp.org.notes.hut.android.MainActivity
 import com.hutapp.org.notes.hut.android.R
 
 const val NOTIFICATION_CHANEL_ID = "my chanel"
 const val NOTIFICATION_NAME = "my notification"
+
 class MyNotification(private val context: Context) {
     private val manager =
         context.getSystemService(NotificationManager::class.java)
 
     fun showNotification(labelNotification: String, idNotification: Int = 1) {
-        //todo else notification
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val chanel = NotificationChannel(
-                NOTIFICATION_CHANEL_ID,
-                NOTIFICATION_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
+        val chanel = NotificationChannel(
+            NOTIFICATION_CHANEL_ID,
+            NOTIFICATION_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
 
-            manager.createNotificationChannel(chanel)
+        manager.createNotificationChannel(chanel)
 
-            val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANEL_ID)
-                .setContentTitle(labelNotification)
-                .setSmallIcon(R.drawable.baseline_edit_note_24)
-                .build()
+        val intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
-            manager.notify(idNotification, notification)
-        }
+        val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANEL_ID)
+            .setContentTitle(labelNotification)
+            .setSmallIcon(R.drawable.baseline_edit_note_24)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        manager.notify(idNotification, notification)
     }
 }
