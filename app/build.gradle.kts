708327d9808c278d912e046ca6d1948a0609ec5e
ml1413 <ml1413sam@gmail.com>
2024-05-02 13:50:39 +0300

    fix bag notification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.05"
+        versionName = "1.06"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
