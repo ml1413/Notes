7531c07606a4a571eac367d020a98cca12f46d5d
ml1413 <ml1413sam@gmail.com>
2024-05-02 13:49:33 +0300

    fix bag notification

--- app/src/main/java/com/hutapp/org/notes/hut/android/notification/MyNotification.kt
+++ app/src/main/java/com/hutapp/org/notes/hut/android/notification/MyNotification.kt
@@ -5,7 +5,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.hutapp.org.notes.hut.android.MainActivity
 import com.hutapp.org.notes.hut.android.R
@@ -32,9 +31,9 @@ class MyNotification(private val context: Context) {
         intent.putExtra(ID_ENTITY, idNotification)
         val pendingIntent = PendingIntent.getActivity(
             context,
-            0,
+            idNotification,
             intent,
-            PendingIntent.FLAG_UPDATE_CURRENT
+            PendingIntent.FLAG_IMMUTABLE
         )
 
         val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANEL_ID)
