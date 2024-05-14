f1f05f17a5d7aba5351ae4094a96bc3f0311431b
ml1413 <ml1413sam@gmail.com>
2024-05-14 21:56:17 +0300

    fix bag drive initialization

--- app/src/main/java/com/hutapp/org/notes/hut/android/ui/tabRow/ModelTabRowItem.kt
+++
@@ -1,5 +0,0 @@
-package com.hutapp.org.notes.hut.android.ui.tabRow
-
-data class ModelTabRowItem(
-    val title: String,
-)
---
+++ app/src/main/java/com/hutapp/org/notes/my_organization/android/ui/tabRow/ModelTabRowItem.kt
@@ -0,0 +1,5 @@
+package com.hutapp.org.notes.my_organization.android.ui.tabRow
+
+data class ModelTabRowItem(
+    val title: String,
+)
